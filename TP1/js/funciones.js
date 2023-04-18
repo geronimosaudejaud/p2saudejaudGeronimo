1bd7ef7889c03eeaedfcfb1e919a3c4b930ef842
geronimosaudejaud <129581599+geronimosaudejaud@users.noreply.github.com>
2023-04-18 18:31:31 -0300

    Add files via upload

---
+++ TP1/js/funciones.js
@@ -0,0 +1,6 @@
+console.log("funciona");
+function alerta(){
+    const aux1=document.getElementById("apellido")
+    const aux2=document.getElementById("nombre")
+    const aux3=document.getElementById("edad")
+    alert(aux1.value+" "+aux2.value+" "+aux3.value)}
